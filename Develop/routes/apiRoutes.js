0f584385164bf5d0f2100ff28bc57afb9a17bc4e
Dustin LaMarr <dustin.lamarr@gmail.com>
2020-11-30 12:24:47 -0700

    saves to db file now

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,32 +3,32 @@ const fs = require("fs");
 
 var noteData = require("../db/db.json");
 
-module.exports = function(app) {
-    app.get("/api/notes", function(req, res) {
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
         res.json(noteData);
-      });
+    });
 
-      app.post("/api/notes", function(req, res) {
+    app.post("/api/notes", function (req, res) {
 
         noteData.push(req.body)
         res.json(noteData)
-        fs.writeFile("db/db.json", JSON.stringify(notesData), function(err, log){
-            if (err){
-                throw err
-            } else {
-                res.json(true)
-            }
+        fs.writeFile("db/db.json", JSON.stringify(noteData), function (err, log) {
+            // if (err) {
+            //     throw err
+            // } else {
+            //     res.json(true)
+            // }
         })
     });
 
-    app.delete("/api/notes/:id", function(req, res){
-        const deleteNote = noteData.filter(note => note.id !== req.params.id);
-        fs.writeFile("db/db.json", JSON.stringify(deleteNote), function(err, log){
-            if (err){
-                throw err
-            } else {
-                res.json(true)
-            }
+    app.delete("/api/notes/:id", function (req, res) {
+        const deleteNote = noteData.filter(notes => notes.id !== req.params.id);
+        fs.writeFile("db/db.json", JSON.stringify(deleteNote), function (err, log) {
+            // if (err) {
+            //     throw err
+            // } else {
+            //     res.json(true)
+            // }
         })
 
     })
